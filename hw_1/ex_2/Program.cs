a572f0515df52fa217cf10686724e7322f9ac64e
Tanya <felisitas@bk.ru>
2023-08-21 18:56:38 +0700

    add ex2

---
+++ hw_1/ex_2/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write ("Введите число 1: ");
+int number1 = int.Parse(Console.ReadLine()!);
+
+Console.Write ("Введите число 2: ");
+int number2 = int.Parse(Console.ReadLine()!);
+
+if (number1 > number2) Console.WriteLine($"Большее число {number1}, а меньшее число {number2}");
+else Console.WriteLine($"Большее число {number2}, а меньшее число {number1}");
