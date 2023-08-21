70f0ef295522298c186e8e63ffe9e9fe60e8eb29
Tanya <felisitas@bk.ru>
2023-08-21 19:12:17 +0700

    add ex4

---
+++ hw_1/ex_4/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write ("Введите число 1: ");
+int number1 = int.Parse(Console.ReadLine()!);
+
+Console.Write ("Введите число 2: ");
+int number2 = int.Parse(Console.ReadLine()!);
+
+Console.Write ("Введите число 3: ");
+int number3 = int.Parse(Console.ReadLine()!);
+
+int max = number1;
+
+if (number2 > max) max = number2;
+if (number3 > max) max = number3;
+
+Console.WriteLine($"Максимальным числом из числел {number1}, {number2}, {number3} является число {max}");
