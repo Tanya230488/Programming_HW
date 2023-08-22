6d89b600b08fee05440c663a03f7c475172c7ab3
Tanya <felisitas@bk.ru>
2023-08-22 19:49:03 +0700

    add ex8

---
+++ hw_1/ex_8/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write ("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+int count = 0;
+
+while (count < number)
+{
+    if (number % 2 == 0)
+    {
+        Console.Write ($"{count + 2}, ");
+    }    
+    else number = number - 1;
+    count = count + 2;
+}    
