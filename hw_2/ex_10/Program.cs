3c561444bd95361ee2836a919ab0b84a3abe6d44
Tanya <felisitas@bk.ru>
2023-08-23 18:00:08 +0700

    add ex 10

---
+++ hw_2/ex_10/Program.cs
@@ -0,0 +1,20 @@
+﻿int InputNum(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+int FindSecond (int num)
+{    
+     return num / 10 % 10;
+}
+
+void PrintSecond (int num)
+{
+    Console.WriteLine($"Вторым числом в числе является число {num}");
+}
+
+int number = InputNum ("Введите трехзначное число: ");
+int secondNumber = FindSecond(number);
+PrintSecond(secondNumber);
+
