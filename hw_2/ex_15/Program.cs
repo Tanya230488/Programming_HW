c9e2f8420ab0ddbfafb6e6d3ce2e1be3f060e9c8
Tanya <felisitas@bk.ru>
2023-08-24 11:04:30 +0700

    add 15 ex

---
+++ hw_2/ex_15/Program.cs
@@ -0,0 +1,22 @@
+﻿int InputNum(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+string CheckWeekend (int num)
+{
+    if (num < 6)
+        return "Нет";
+    else 
+        return "Да";    
+}
+
+void PrintRes (string res)
+{
+    Console.WriteLine(res);
+}
+
+
+int number = InputNum("Введите число: ");
+PrintRes (CheckWeekend(number));
