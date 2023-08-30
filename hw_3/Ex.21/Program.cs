06430310f1b8f760f0fd5b96fae86757941b9c2f
Tanya <felisitas@bk.ru>
2023-08-30 23:00:21 +0700

    add ex 19, 23

---
+++ hw_3/Ex.21/Program.cs
@@ -0,0 +1,27 @@
+﻿int[] Coor(string msg)
+{
+    int[] coordinates = new int[3];
+    Console.Write($"{msg} X: ");
+    coordinates[0] = int.Parse(Console.ReadLine()!);
+
+    Console.Write($"{msg} Y: ");
+    coordinates[1] = int.Parse(Console.ReadLine()!);
+
+    Console.Write($"{msg} Z: ");
+    coordinates[2] = int.Parse(Console.ReadLine()!);
+
+    return coordinates;
+}
+
+double Distance (int[] coorA, int[] coorB)
+{
+    double res = Math.Sqrt(Math.Pow((coorB[0] - coorA[0]),2) + Math.Pow((coorB[1] - coorA[1]),2) + Math.Pow((coorB[2] - coorA[2]),2));
+    return res;
+}
+
+int[] coorA = Coor("Введите для точки А координату");
+int[] coorB = Coor("Введите для точки B координату");
+
+double dist = Distance (coorA, coorB);
+Console.Write(dist);
+
---
+++ hw_3/Ex.23/Program.cs
@@ -0,0 +1,22 @@
+﻿int InputNum (string msg)
+{
+    Console.Write(msg);
+    int num = int.Parse(Console.ReadLine()!);
+    return num;
+}
+
+void Cubes(int num)
+{
+    for(int i = 1; i < num; i++)
+    {
+        int cube = (int) Math.Pow(i, 3);
+        Console.Write($"{cube}, ");
+
+    }
+
+    int cube2 = (int) Math.Pow(num, 3);
+    Console.Write($"{cube2}");
+}
+
+int num = InputNum("Введите число: ");
+Cubes(num);
