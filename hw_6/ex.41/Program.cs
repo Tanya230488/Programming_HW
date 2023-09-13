589a9696d583a8a4e864d60e8ce363428679ec03
Tanya <felisitas@bk.ru>
2023-09-13 11:49:14 +0700

    add hw 6

---
+++ hw_6/ex.41/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int ImputNum(string msg)
+{
+    Console.Write(msg);
+    return int.Parse(Console.ReadLine()!);
+}
+
+int[] CreateAr (int size)
+{
+    return new int[size];
+}
+
+void FillArray (int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(-100, 101);
+}
+
+void PrintAr (int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+    Console.WriteLine();
+}
+
+string CountPosDigit(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0)
+            count++;
+    }
+    return $"Количество чисел больше нуля равно {count}";
+}
+
+
+int size = ImputNum("Введите размер массива: ");
+int[] arr = CreateAr(size);
+FillArray(arr);
+PrintAr(arr);
+Console.WriteLine(CountPosDigit(arr));
---
+++ hw_6/ex.43/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+int ImputNum(string msg)
+{
+    Console.Write(msg);
+    return int.Parse(Console.ReadLine()!);
+}
+
+void Cheking (int b1, int b2, int k1, int k2)
+{
+    if (k1 == k2)
+    return "Прямые не пересекаются";
+
+    if (k1 == k2 && b1 == b2)
+    return "Прямые совпадают";
+}
+
+double[] PointOfMeet (int b1, int b2, int k1, int k2)
+{
+    double[] arr = new[2];
+    arr[0] = (b2-b1)/(k1-k2);
+    arr[1] = k1 * arr[0] + b1;     
+
+    return arr[];    
+}
+
+void PrintAr (int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+    Console.WriteLine();
+}
+
+int b1 = ImputNum("Введите значение b1: ");
+int b2 = ImputNum("Введите значение b2: ");
+int k1 = ImputNum("Введите значение k1: ");
+int k2 = ImputNum("Введите значение k2: ");
+Console.WriteLine(PointOfMeet(b1, b2, k1, k2));
